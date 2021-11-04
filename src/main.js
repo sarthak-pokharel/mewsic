17acc1ee1477e5b34b1ada35ecdd952233f8481d
Sarthak <sarthak.whenever@gmail.com>
2021-11-04 18:47:56 +0545

    yey deployable

--- src/main.js
+++ src/main.js
@@ -6,26 +6,21 @@ import $ from 'jquery';
 
 $(Main);
 
-let songsList = null, ci = 0;
-
-const audioQuality = "max";
-
+let songsList = null, ci = 0, audioQuality = "max";
 let loadedurl = false;
-
-
-let serverInfo = new URL('https://localhost:3000/');
+//music server
+let serverInfo = new URL('http://localhost:8080/');
 serverInfo.host = "localhost";
 serverInfo.port = "8080";
 serverInfo.protocol = "http";
 
-
-
 async function Main(){
 	let songs = await loadSongs();
-	// console.log(songs);
-	// await playSong();
+	songsList = songs;
 	hideLoader();
 };
+
+const hideEl = ($el,f=false)=> f?$el.removeClass('hidden'):$el.addClass('hidden');
 
 const showLoader = ()=> [hideEl($(".loader"), true), hideEl($(".music-app"))];
 const hideLoader = ()=> [hideEl($(".loader")), hideEl($(".music-app"), true)];
@@ -35,27 +30,27 @@ const hideLoader = ()=> [hideEl($(".loader")), hideEl($(".music-app"), true)];
 $("#next").click(()=>playNext());
 $("#previous").click(()=>playPrev());
 
-function playNext(){
+const playNext = ()=> {
 	stopMusic();
 	loadedurl = false;
 	if(ci < songsList.length-1) ci++;
 	else ci = 0;
 	playMusic();
 }
-function playPrev(){
+const playPrev = ()=>{
 	loadedurl = false;
 	if(ci>0) ci--;
 	else ci = songsList.length-1;
 	playMusic();
 }
 
-function chooseQuality(songflist){
+const chooseQuality = songflist=>{
 	if(audioQuality == "max") return songflist[songflist.length-1];
 	else if(audioQuality == "min") return songflist[0];
 	return songflist[songflist.length-1];
 }
 
-async function loadSong(){
+const loadSong = async ()=>{
 	showLoader();
 	let {flt,thumbnails} = await getYtSongDetail(songsList[ci]);
 	let songd = chooseQuality(flt);
@@ -66,9 +61,9 @@ async function loadSong(){
 }
 
 
-const hideEl = ($el,f=false)=> f?$el.removeClass('hidden'):$el.addClass('hidden');
 
-function getYtSongDetail(ytUrl){
+
+const getYtSongDetail = (ytUrl)=>{
 	return new Promise((res,rej)=>{
 		$.post(serverInfo.toString() + "getYtSongDetail", {ytUrl}, function(data){
 			let flt = data.formats
@@ -79,117 +74,69 @@ function getYtSongDetail(ytUrl){
 	})
 }
 
-function loadSongs(songs){
+const loadSongs = function (songs){
 	return new Promise((res,rej)=>{
-		$.get(serverInfo.toString()+"getMusicList?playlist="+encodeURI('https://www.youtube.com/playlist?list=PLsFgdBwHAdmY1GmdwH3DhCk8BX8ND4-tl'), function(data){
-			songsList = data;
-			res(data);
-		});
+		$.get(serverInfo.toString()+
+				"getMusicList?playlist="+
+				encodeURI('https://www.youtube.com/playlist?list=PLsFgdBwHAdmY1GmdwH3DhCk8BX8ND4-tl'), 
+			data=>res(data));
 	})
 }
 
 
+let cover = $(".cover")[0];
+let audio = $("#audioplayer")[0];
+let icon = $("#play .icons")[0];
 
-
-var btnPlay = document.getElementById("play");
-var btnStop = document.getElementById("stop");
-var btnReplay = document.getElementById("replay");
-//Image
-var cover = document.querySelector(".cover");
-//Element audio
-var audio = document.getElementById("audioplayer");
-//Icon play and pause
-var icon = document.querySelector("#play .icons");
-//Progress Bar
-var progressBar = document.getElementById("progress-bar");
-var progress = document.getElementById("progress");
-
-var count = 0;
+let count = 0;
 let playing = false;
 
-async function playMusic() {
-	await loadSong();
+const playMusic = async ()=> {
+	if(!loadedurl) await loadSong();
 	cover.setAttribute("class", "play");
 	icon.setAttribute("class", "fas fa-pause");
-	audio.play();
-	playing = true;
-	//Count the clicks
-	count += 1;
+	audio.play(); playing = true; count += 1;
 }
 
-//---------------- Play music
-btnPlay.addEventListener("click", () => {
-	if(playing == false){
-		playMusic();
-	}else {
-		pauseMusic();
-	}
-});
 
-function pauseMusic(){
+const pauseMusic = ()=>{
 	cover.setAttribute("class", "cover");
 	icon.setAttribute("class", "fas fa-play");
 	audio.pause();
 	playing = false;
 }
 
-//---------------- Stop music
-btnStop.addEventListener("click", () => {
-	stopMusic();
-	count = 0;
+$("#play").click(() => playing?pauseMusic():playMusic());
+$("#stop").click(() => [[stopMusic(),count = 0]]);
+$("#replay").click(() => audio.currentTime = audio.currentTime - 10);
+
+
+$(audio).on("ended", ()=> $(".btn-nextt").trigger('click'));
+
+$(audio).on("timeupdate", ()=> {
+	if(Number.isNaN(audio.duration)) return;
+	$("#duration")[0].innerHTML = convertElapsedTime(audio.duration);
+	$("#current-time")[0].innerHTML = convertElapsedTime(audio.currentTime);
+	$("#progress")[0].style.width = (audio.currentTime / audio.duration) * 100 + "%";
+	if (audio.currentTime >= audio.duration) stopMusic();
 });
 
-//---------------- Replay music
-btnReplay.addEventListener("click", () => {
-	var time = audio.currentTime;
-	var replay_10 = time - 10;
-	audio.currentTime = replay_10;
-});
 
-//---------------- Audio
-
-audio.addEventListener("ended", function () {
-	console.log("Song ended");
-	$(".btn-nextt").trigger('click')
-})
-audio.addEventListener("timeupdate", function () {
-	//Time
-	var duration = audio.duration;
-	if(Number.isNaN(duration)) return;
-	var currentTime = audio.currentTime;
-	document.getElementById("duration").innerHTML = convertElapsedTime(duration);
-	document.getElementById("current-time").innerHTML = convertElapsedTime(
-		currentTime
-	);
-	//Progress Bar
-	progress.style.width = (currentTime / duration) * 100 + "%";
-
-	//Stop when audio ends
-	if (currentTime >= duration) {
-		stopMusic();
-	}
-});
-
-//---------------- Control of the progress
-progressBar.addEventListener("mousedown", function (event) {
-	var clickedPosition = event.clientX - event.target.offsetLeft;
+$("#progress-bar").on("mousedown", (event)=> {
 	audio.currentTime =
-		(clickedPosition / event.target.offsetWidth) * audio.duration;
-});
+		((event.clientX - event.target.offsetLeft) / event.target.offsetWidth) * audio.duration;
+});	
 
-//Convert
-function convertElapsedTime(inputSeconds) {
-	// console.log(inputSeconds)
-	var seconds = Math.floor(inputSeconds % 60);
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var minutes = Math.floor(inputSeconds / 60);
+
+const convertElapsedTime = (inputSeconds)=> {
+	let seconds = Math.floor(inputSeconds % 60);
+	if (seconds < 10) seconds = "0" + seconds;
+	let minutes = Math.floor(inputSeconds / 60);
 	return minutes + ":" + seconds;
 }
 
-//Function stop
-function stopMusic() {
+
+const stopMusic = () => {
 	audio.pause();
 	audio.currentTime = 0;
 	icon.setAttribute("class", "fas fa-play");
